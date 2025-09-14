920b64530318baddd4e8b0e01f896b911477b058
Tom Boone <tbc@tomboone.com>
2025-09-14 18:43:10 -0400

    add terraform-build-deploy github actions workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,18 @@
 variable "location" {
+  type        = string
   description = "Azure region for resources"
+}
+ variable "log_analytics_workspace_name" {
+   type        = string
+   description = "Existing log analytics workspace to use"
+ }
+
+variable "log_analytics_workspace_rg_name" {
   type        = string
-  default     = "East US"
+  description = "Resource group of the existing log analytics workspace"
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "production"
-}
-
-variable "custom_domain" {
-  description = "Custom domain name (optional)"
-  type        = string
-  default     = null
+variable "api_base_url" {
+  type = string
+  description = "Base URL of the backend API"
 }
