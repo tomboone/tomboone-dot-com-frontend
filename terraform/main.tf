8e9383bfeb666bb1d3ceaf620027f97d5f4e30fa
Tom Boone <tbc@tomboone.com>
2025-09-14 18:43:10 -0400

    add terraform-build-deploy github actions workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,31 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
+locals {
+  service_name = "tomboone-dot-com-frontend"
 }
 
-provider "azurerm" {
-  features {}
+data "azurerm_log_analytics_workspace" "existing" {
+  name                = var.log_analytics_workspace_name
+  resource_group_name = var.log_analytics_workspace_rg_name
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "rg-tomboone-frontend"
-  location = "East US"
+  name     = "${local.service_name}-rg"
+  location = var.location
 }
 
-resource "azurerm_static_site" "main" {
-  name                = "swa-tomboone-frontend"
+resource "azurerm_application_insights" "main" {
+  name                = "${local.service_name}-insights"
   resource_group_name = azurerm_resource_group.main.name
-  location           = azurerm_resource_group.main.location
-  sku_tier           = "Free"
-  sku_size           = "Free"
+  location            = azurerm_resource_group.main.location
+  workspace_id        = data.azurerm_log_analytics_workspace.existing.id
+  application_type    = "web"
+}
 
-  tags = {
-    environment = "production"
-    project     = "tomboone-frontend"
+resource "azurerm_static_web_app" "main" {
+  name                = local.service_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  app_settings = {
+    "VITE_API_BASE_URL" = var.api_base_url
   }
-}
-
-# Optional: Custom domain
-resource "azurerm_static_site_custom_domain" "main" {
-  static_site_id  = azurerm_static_site.main.id
-  domain_name     = "tomboone.com"
-  validation_type = "cname-delegation"
-}
-
-output "static_web_app_url" {
-  value = azurerm_static_site.main.default_host_name
-}
-
-output "static_web_app_id" {
-  value = azurerm_static_site.main.id
 }
