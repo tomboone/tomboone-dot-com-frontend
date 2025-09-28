72b09fdaf56ef76dfd267e8ae7e37c3663b96e49
Tom Boone <tbc@tomboone.com>
2025-09-27 21:43:59 -0400

    remove app insights from terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,12 +2,3 @@ variable "location" {
   type        = string
   description = "Azure region for resources"
 }
- variable "log_analytics_workspace_name" {
-   type        = string
-   description = "Existing log analytics workspace to use"
- }
-
-variable "log_analytics_workspace_rg_name" {
-  type        = string
-  description = "Resource group of the existing log analytics workspace"
-}
