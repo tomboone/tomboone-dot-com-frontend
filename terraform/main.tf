fb159718514fc5c32283cc7d154fb55c9d7a639d
Tom Boone <tbc@tomboone.com>
2025-09-27 21:43:59 -0400

    remove app insights from terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,22 +2,9 @@ locals {
   service_name = "tomboone-dot-com-frontend"
 }
 
-data "azurerm_log_analytics_workspace" "existing" {
-  name                = var.log_analytics_workspace_name
-  resource_group_name = var.log_analytics_workspace_rg_name
-}
-
 resource "azurerm_resource_group" "main" {
   name     = "${local.service_name}-rg"
   location = var.location
-}
-
-resource "azurerm_application_insights" "main" {
-  name                = "${local.service_name}-insights"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  workspace_id        = data.azurerm_log_analytics_workspace.existing.id
-  application_type    = "web"
 }
 
 resource "azurerm_static_web_app" "main" {
